590135f18646fd52fa217f86cf220006f75cace8
Mahmoud Abdulaal <mmh.00090@gmail.com>
2025-03-25 22:53:17 +0100

    simple makefile

---
+++ led.h
@@ -0,0 +1,10 @@
+#ifndef LED_H
+#define LED_H
+
+#include <stdint.h>
+
+// Function prototypes
+void LED_Init(void);          // Initialize LED pins
+void LED_ToggleAll(void);     // Toggle all LEDs
+
+#endif // LED_H
